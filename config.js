19d7a0840f98439c07d98ebd7fc90e1c790a7ce7
byjus-proj-prop <70767789+byjus-proj-prop@users.noreply.github.com>
2022-09-04 14:37:55 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyCK1vmWIXUliKm21aYsgHDjkbkryqyoR9w",
+    authDomain: "narracao-271e5.firebaseapp.com",
+    databaseURL: "https://narracao-271e5-default-rtdb.firebaseio.com",
+    projectId: "narracao-271e5",
+    storageBucket: "narracao-271e5.appspot.com",
+    messagingSenderId: "195785973620",
+    appId: "1:195785973620:web:58daf459a12372d6ef9621"
+  };
